01cfa5ca446b3e5c46bcbf4ca61245be59e4b4b8
semil <semiloreomotade@gmail.com>
2026-03-30 05:24:41 +0100

    add: create election activate test

--- src/test/java/dreamVote/dreamdev/services/ElectionServiceImplTest.java
+++ src/test/java/dreamVote/dreamdev/services/ElectionServiceImplTest.java
@@ -5,6 +5,7 @@ import dreamVote.dreamdev.data.repositories.VoterRepository;
 import dreamVote.dreamdev.dtos.requests.CreateElectionRequest;
 import dreamVote.dreamdev.dtos.requests.LoginRequest;
 import dreamVote.dreamdev.dtos.requests.VoterRegisterationRequest;
+import dreamVote.dreamdev.dtos.responses.CreateElectionResponse;
 import dreamVote.dreamdev.dtos.responses.VoterRegisterationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -55,13 +56,24 @@ class ElectionServiceImplTest {
     }
 
     @Test
-    public void createElection_ActiveSuccessTest(){
+    public void createElection_isNotActiveTest(){
         String voterId = voterRepository.findByEmail("email").get().getId();
         CreateElectionRequest createElectionRequest = new CreateElectionRequest();
         createElectionRequest.setVoterId(voterId);
-        assertEquals(0L, electionRepository.count());
-        electionService.createElection(createElectionRequest);
-        assertEquals(1L, electionRepository.count());
-        electionService.activateElection(
+        CreateElectionResponse response = electionService.createElection(createElectionRequest);
+        String electionId = response.getElectionId();
+        assertFalse(electionRepository.findById(electionId).get().isActive());
+    }
+
+    @Test
+    public void createElection_activateElectionTest(){
+        String voterId = voterRepository.findByEmail("email").get().getId();
+        CreateElectionRequest createElectionRequest = new CreateElectionRequest();
+        createElectionRequest.setVoterId(voterId);
+        CreateElectionResponse response = electionService.createElection(createElectionRequest);
+        String electionId = response.getElectionId();
+        assertFalse(electionRepository.findById(electionId).get().isActive());
+        electionService.activate(electionId);
+        assertTrue(electionRepository.findById(electionId).get().isActive());
     }
 }
